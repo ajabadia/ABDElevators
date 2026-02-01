06497cfb104118770ff868118f9c984e623a21ce
Alejandro Abadía <ajabadia@gmail.com>
2026-02-01 13:02:59 +0100

    fix(auth-route): explicit export of handlers and restore imports

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,14 @@
+import { NextRequest } from "next/server";
 import { handlers } from "@/lib/auth";
 
-export const { GET, POST } = handlers;
+const { GET: AuthGET, POST: AuthPOST } = handlers;
+
+export async function GET(request: NextRequest) {
+    // console.log("🔥 [API/AUTH] GET Request:", request.url);
+    return AuthGET(request);
+}
+
+export async function POST(request: NextRequest) {
+    // console.log("🔥 [API/AUTH] POST Request:", request.url);
+    return AuthPOST(request);
+}
